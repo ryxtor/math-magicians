6211fffa5b9c827e2b86fddc9343377d48525715
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-03-15 14:13:24 -0300

    Add calculator component and style it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-
+import React from 'react';
 import './App.css';
+import Calculator from './components/Calculator';
 
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-
-
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
