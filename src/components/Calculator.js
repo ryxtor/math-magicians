a4a236870c02ed94a1580d1e2a30fdd6d53b9dc4
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-03-22 16:37:56 -0300

    Add styling to header.js and calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -11,27 +11,30 @@ export default function Calculator() {
   }
 
   return (
-    <div className="calculator-container">
-      <div className="result">{ state.next || state.operation || state.total || 0}</div>
-      <button type="button" onClick={() => click(obj, 'AC')} className="btn btn-ac">AC</button>
-      <button type="button" onClick={() => click(obj, '+/-')} className="btn btn-plus-minus">+/-</button>
-      <button type="button" onClick={() => click(obj, '%')} className="btn btn-percent">%</button>
-      <button type="button" onClick={() => click(obj, '÷')} className="btn btn-divide orange-color">÷</button>
-      <button type="button" onClick={() => click(obj, '7')} className="btn btn-7">7</button>
-      <button type="button" onClick={() => click(obj, '8')} className="btn btn-8">8</button>
-      <button type="button" onClick={() => click(obj, '9')} className="btn btn-9">9</button>
-      <button type="button" onClick={() => click(obj, 'x')} className="btn btn-multiply orange-color">&times;</button>
-      <button type="button" onClick={() => click(obj, '4')} className="btn btn-4">4</button>
-      <button type="button" onClick={() => click(obj, '5')} className="btn btn-5">5</button>
-      <button type="button" onClick={() => click(obj, '6')} className="btn btn-6">6</button>
-      <button type="button" onClick={() => click(obj, '-')} className="btn btn-minus orange-color">-</button>
-      <button type="button" onClick={() => click(obj, '1')} className="btn btn-1">1</button>
-      <button type="button" onClick={() => click(obj, '2')} className="btn btn-2">2</button>
-      <button type="button" onClick={() => click(obj, '3')} className="btn btn-3">3</button>
-      <button type="button" onClick={() => click(obj, '+')} className="btn btn-add orange-color">+</button>
-      <button type="button" onClick={() => click(obj, '0')} className="btn btn-0">0</button>
-      <button type="button" onClick={() => click(obj, '.')} className="btn btn-decimal">.</button>
-      <button type="button" onClick={() => click(obj, '=')} className="btn btn-equal orange-color">=</button>
+    <div className="calculator-section">
+      <h2 className="calculator-title">Let`s do some math!</h2>
+      <div className="calculator-container">
+        <div className="result">{ state.next || state.operation || state.total || 0}</div>
+        <button type="button" onClick={() => click(obj, 'AC')} className="btn btn-ac">AC</button>
+        <button type="button" onClick={() => click(obj, '+/-')} className="btn btn-plus-minus">+/-</button>
+        <button type="button" onClick={() => click(obj, '%')} className="btn btn-percent">%</button>
+        <button type="button" onClick={() => click(obj, '÷')} className="btn btn-divide orange-color">÷</button>
+        <button type="button" onClick={() => click(obj, '7')} className="btn btn-7">7</button>
+        <button type="button" onClick={() => click(obj, '8')} className="btn btn-8">8</button>
+        <button type="button" onClick={() => click(obj, '9')} className="btn btn-9">9</button>
+        <button type="button" onClick={() => click(obj, 'x')} className="btn btn-multiply orange-color">&times;</button>
+        <button type="button" onClick={() => click(obj, '4')} className="btn btn-4">4</button>
+        <button type="button" onClick={() => click(obj, '5')} className="btn btn-5">5</button>
+        <button type="button" onClick={() => click(obj, '6')} className="btn btn-6">6</button>
+        <button type="button" onClick={() => click(obj, '-')} className="btn btn-minus orange-color">-</button>
+        <button type="button" onClick={() => click(obj, '1')} className="btn btn-1">1</button>
+        <button type="button" onClick={() => click(obj, '2')} className="btn btn-2">2</button>
+        <button type="button" onClick={() => click(obj, '3')} className="btn btn-3">3</button>
+        <button type="button" onClick={() => click(obj, '+')} className="btn btn-add orange-color">+</button>
+        <button type="button" onClick={() => click(obj, '0')} className="btn btn-0">0</button>
+        <button type="button" onClick={() => click(obj, '.')} className="btn btn-decimal">.</button>
+        <button type="button" onClick={() => click(obj, '=')} className="btn btn-equal orange-color">=</button>
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <h1>Math Magicians</h1>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="calculator">Calculator</Link>
-      <Link to="quote">Quote</Link>
+    <h1 className="header-title">Math Magicians</h1>
+    <nav className="navbar">
+      <Link to="/" className="link">Home</Link>
+      <Link to="calculator" className="link">Calculator</Link>
+      <Link to="quote" className="link">Quote</Link>
     </nav>
   </header>
 );
